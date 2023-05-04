401e03e019fc2065882c944a64b65b261bb2483b
rpm <ryan.macarthur@gmail.com>
2023-05-04 16:28:40 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(() => {
+	return {
+		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		preview: {
+			headers: {
+				"Cache-Control": "public, max-age=600"
+			}
+		},
+		resolve: {
+			alias: {
+			  ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+			}
+		  }
+	};
+});
